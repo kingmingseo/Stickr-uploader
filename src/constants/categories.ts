cdbf31e33448b5ed759e2dd708bae85d7794e4df
kingmingseo <multi1971@mju.ac.kr>
2025-10-15 17:50:04 +0900

    fix : 카테고리 수정

--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -1,13 +1,11 @@
 export const categoriesMap: Record<string, string> = {
   전체: '전체',
+  손글씨: 'handwriting',
   도트: 'dot',
   말풍선: 'speech-bubble',
   캐릭터: 'character',
   이모지: 'emoji',
-  감정: 'emotion',
-  여행: 'travel',
   음식: 'food',
-  스포츠: 'sports',
 };
 
 // 한국어 → 영어 변환
